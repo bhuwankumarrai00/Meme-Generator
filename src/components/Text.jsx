b2258581a90c9a07456abcd399ce14b2082a418f
Bhuwan Kumar Rai <88026910+bhuwankumarrai00@users.noreply.github.com>
2024-06-16 17:25:13 +0530

    Update Text.jsx

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,9 +2,10 @@ import React, { useState, useRef, useEffect } from "react";
 import Draggable from "react-draggable";
 
 const Text = () => {
-  const [editMode, setEditMode] = useState(false);
-  const [val, setVal] = useState("Double Click to Edit");
+  const [isEditing, setIsEditing] = useState(false);
+  const [val, setVal] = useState("Click to Edit");
   const [fontSize, setFontSize] = useState(16); // Default font size
+  const [textColor, setTextColor] = useState("#000"); // Default text color
 
   const inputRef = useRef(null);
 
@@ -13,12 +14,15 @@ const Text = () => {
     borderRadius: "5px",
     padding: "5px",
     fontSize: `${fontSize}px`,
+    color: textColor,
+    cursor: "pointer",
   };
 
   const headingStyle = {
     textAlign: "center",
     fontSize: `${fontSize}px`,
     cursor: "pointer",
+    color: textColor,
   };
 
   const increaseFontSize = () => {
@@ -29,11 +33,24 @@ const Text = () => {
     setFontSize((prevSize) => Math.max(prevSize - 2, 10)); // Ensure minimum font size
   };
 
+  const randomColor = () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setTextColor(randomColor);
+  };
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+
   useEffect(() => {
     const handleClickOutside = (e) => {
       // Check if clicked element is not inside the input field
       if (inputRef.current && !inputRef.current.contains(e.target)) {
-        setEditMode(false);
+        setIsEditing(false);
       }
     };
 
@@ -45,23 +62,25 @@ const Text = () => {
 
   return (
     <Draggable>
-      {editMode ? (
+      {isEditing ? (
         <div ref={inputRef}>
           <input
             style={inputStyle}
-            onDoubleClick={(e) => setEditMode(false)}
+            autoFocus // Auto focus on input when in edit mode
+            onBlur={handleBlur} // Exit edit mode on blur
             value={val}
             onChange={(e) => setVal(e.target.value)}
           />
           <div>
             <button onClick={increaseFontSize}>Increase Font Size</button>
             <button onClick={decreaseFontSize}>Decrease Font Size</button>
+            <button onClick={randomColor}>Random Color</button>
           </div>
         </div>
       ) : (
         <h1
           style={headingStyle}
-          onDoubleClick={(e) => setEditMode(true)}
+          onClick={handleDoubleClick} // Toggle edit mode on click
           ref={inputRef}
         >
           {val}
